79509a2a05ba28d584de240594ed2cb073221045
mkolek1015 <mkolek7@yahoo.com>
2020-08-31 23:22:44 -0400

    updated readme

---
+++ assests/questions.js
@@ -0,0 +1,54 @@
+var jsQuestions = [
+    {
+      title: "How many laps must a driver survive to win the Indianapolis 500?",
+      choices: ["500", "450", "350", "200"],
+      answer: "200"
+    },
+    {
+      title: "Which country were the 1932 Winter Olympic Games held in?",
+      choices: ["USA", "SWEDEN", "USSR", "JAPAN"],
+      answer: "USA"
+    },
+    {
+      title: "What HTML stand for?",
+      choices: ["Ham, Tomato, Mayo, Lettuce", "Hypertext Markup Language", "Hold The MaiL", "Hackers Teaching Machines to Learn"],
+      answer: "Hypertext Markup Language"
+    },
+    {
+      title: "What is short for Light amplification by stimulated emission of radiation?",
+      choices: ["LAMP", "LIGHT RADIATION", "LIGHT SPEED", "LASER"],
+      answer: "LASER"
+    },
+    {
+      title: "How many flat faces does a dodecahedron have?",
+      choices: ["12", "22", "32", "20"],
+      answer: "12"
+    },
+    {
+      title: "What is celebrated in February and was also made into a film?",
+      choices: ["Valentines Day", "Ground Hogs Day", "Mardi Gras", "Nivana Day"],
+      answer: "Ground Hogs Day"
+    },
+    {
+      title: "What is the only vowel on a standard keyboard that is not on the top line of letters?",
+      choices: ["A", "E", "I", "O"],
+      answer: "A"
+    },
+    {
+      title: "What kind of animal is the title character in the 2003 movie Finding Nemo?",
+      choices: ["Yellow Tang", "Pufferfish", "Moorish Idol", "Clownfish"],
+      answer: "Clownfish"
+    },
+    {
+      title: "Due to getting sick, which astronaut was replaced in the Apollo XIII mission?",
+      choices: ["Buzz Aldrin", "Neil Armstrong", "Ken Mattingly", "Buck Rogers"],
+      answer: "Ken Mattingly"
+    },
+    {
+      title: "Who provided the original voice for the Genie in Aladdin?",
+      choices: ["Barbara Eden", "Will Smith", "Robin Williams", "James Iglehart"],
+      answer: "Robin Williams"
+    },
+    
+    
+]
